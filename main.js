c3d9543f6274d45378dac017a61694895db7cc97
priyankacute <bhavanimurugesh2000@gmail.com>
2023-09-22 10:17:17 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,14 @@
+var a=[1,4,3,4,5,6];
+var b=[new Set(a)];
+console.log(b);
+
+/*
+for (let i=0; i<a.length;i++){
+    for (let j=0;j<a.length;j++){
+    if(a[i]==a[j]){
+        b.push(a[i]);
+
+    }
+}
+}
+console.log(a); */
